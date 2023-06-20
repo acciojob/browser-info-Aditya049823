48318855ec2c902edd0596b7f063c30c1264edee
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-06-20 10:59:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+var browserInfo=document.getElementById("browser-info");
+var v="You are using" + navigator.appName + " "+ "version" + navigator.appVersion;
+browserInfo.innerHTML=v;
